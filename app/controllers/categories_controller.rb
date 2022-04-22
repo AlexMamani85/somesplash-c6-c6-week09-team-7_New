5f5310b0102a31ef2ae48009bc0f04fa9ae3fa73
Revocod3 <kevinlopez2693@gmail.com>
2022-04-22 18:15:44 -0500

    ADD: Destroy controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,20 @@
 class CategoriesController < ApplicationController
+  # GET /cagegories
   def index
     @categories = Category.all
+  end
+
+  # DELETE /categories/:id
+  def destroy
+    @categories = Category.find(params[:id])
+    @categories.destroy
+    
+    redirect_to categories_path, status: :see_other
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description ) #:cover # cover after implement ActiveAttachment
   end
 end
