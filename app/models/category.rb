d0c1f82ca46b416199fdb5a550779d1cd6643b67
Revocod3 <kevinlopez2693@gmail.com>
2022-04-23 14:00:29 -0500

    Bug : database correction :L

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
                    uniqueness: true
   validates :description, presence: true,
                           length: { in: 10..240 }
-  validates :cover, presence: true
+  # validates :cover, presence: true
 
   has_many :comments, as: :commentable, dependent: :nullify
   has_many :photos, dependent: :nullify
